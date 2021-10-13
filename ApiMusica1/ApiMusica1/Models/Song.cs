fe5c89c2acb1137c90484f0f7190aab78787834b
MariaIreneRiveroBenavides <a2020113350@estudiantes.upsa.edu.bo>
2021-10-12 20:21:46 -0400

    Agregar archivos de proyecto.

---
+++ ApiMusica1/ApiMusica1/Models/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMusica1.Models
+{
+    public class Song
+    {
+        [Key]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "El autor es requerido")]
+        [StringLength(40, MinimumLength = 2, ErrorMessage = "El nombre del autor debe tener entre 2 y 40 caracteres")]
+        [Display(Name = "Nombre completo del autor")]
+        public string Author { get; set; }
+        [Display(Name = "Letra de la canción")]
+        public string Lyrics { get; set; }
+        [Required]
+        [Url]
+        [Display(Name = "Link de Youtube")]
+        public string Link { get; set; }
+    }
+}
